647972ec56aadcef5f3cad1920ad6379c588c04a
Substitute <Substitute@pocketdevs.org>
2022-10-17 02:24:34 -0400

    additional endpoint

--- Bandai/GevoApi/Client.cs
+++ Bandai/GevoApi/Client.cs
@@ -1,34 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Bandai.GevoApi.Models.Request;
 using RestSharp;
+using RestSharp.Authenticators;
+
 namespace Bandai.GevoApi
 {
-    public interface IClient
-    {
-        public Task LoginAsync(string authentication);
-    }
-    public class SteamClient : IClient
-    {
-        private RestClient _client = new RestClient("https://api.gundamevogame.com/api").AddDefaultHeader("Platform", "Steam");
 
-        public async Task LoginAsync(string token)
+    public abstract class BaseClient
+    {
+        protected abstract RestClient RestClient { get; }
+
+        protected async Task KeepAlive()
         {
-            var otp = await REST.PreLogin(PreLogin.Defaults.Steam with { authToken = "" }, _client);
+            while (true)
+            {
+                await REST.HandShake(RestClient);
+                Debug.WriteLine("Handshake was successful");
+            }
+        }
+        public async Task<Models.Response.SearchFriend> SearchFriend(string name)
+        {
+            var friend = await REST.SearchFriend(new(){ playerName = name }, RestClient);
+            ArgumentNullException.ThrowIfNull(friend);
+            if(friend.IsError)
+                throw new ArgumentException("SearchFriend returned an error.");
+            return friend;
+        }
+        public abstract Task LoginAsync(string authentication);
+    }
+
+    public sealed class SteamClient : BaseClient
+    {
+
+        protected override RestClient RestClient { get; } = new RestClient("https://api.gundamevogame.com/api").AddDefaultHeader("Platform", "Steam");
+
+        public override async Task LoginAsync(string authentication)
+        {
+            var otp = await REST.PreLogin(PreLogin.Defaults.Steam with { authToken = authentication }, RestClient);
             ArgumentNullException.ThrowIfNull(otp);
             if(otp.IsError)
                 throw new ArgumentException("PreLogin returned an error.");
 
-            var jwt = await REST.Login(Login.Defaults.Steam with { authToken = otp.token }, _client);
+            var jwt = await REST.Login(Login.Defaults.Steam with { authToken = otp.token }, RestClient);
             ArgumentNullException.ThrowIfNull(jwt);
             if (jwt.IsError)
                 throw new ArgumentException("Login returned an error");
 
-            
+            RestClient.Authenticator = new JwtAuthenticator(jwt.token);
+
+            _ = Task.Run(KeepAlive); //begin the heartbeat process to keep jwt valid.
         }
+
     }
 }
--- Bandai/GevoApi/Rest.cs
+++ Bandai/GevoApi/Rest.cs
@@ -9,7 +9,28 @@ namespace Bandai.GevoApi
 {
     public static class REST
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="client"></param>
+        /// <returns></returns>
         public static async Task<Models.Response.PreLogin?> PreLogin(Models.Request.PreLogin data, RestClient client) => await client.PostJsonAsync<Models.Request.PreLogin, Models.Response.PreLogin>("Auth/PreLogin", data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="client"></param>
+        /// <returns></returns>
         public static async Task<Models.Response.Login?> Login(Models.Request.Login data, RestClient client) => await client.PostJsonAsync<Models.Request.Login, Models.Response.Login>("Auth/Login", data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static async Task<object?> HandShake(RestClient client) => await client.PostJsonAsync<object, object /* Unimplemented Response Type */ >("HandShake", new { });
+
+        public static async Task<Models.Response.SearchFriend?> SearchFriend(Models.Request.SearchFriend data, RestClient client) => await client.PostJsonAsync<Models.Request.SearchFriend, Models.Response.SearchFriend /* Unimplemented Response Type */ >("Social/SearchFriend",  data);
+
     }
 }
