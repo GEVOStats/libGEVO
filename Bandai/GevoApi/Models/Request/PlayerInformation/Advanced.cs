2a43c1411bb1613d195c88fbfc7baff92d669e83
Substitute <Substitute@pocketdevs.org>
2022-10-24 02:34:42 -0400

    Add PlayerInformation/Advanced Models

---
+++ Bandai/GevoApi/Models/Request/PlayerInformation/Advanced.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bandai.GevoApi.Models.Request.PlayerInformation
+{
+    public record Advanced(
+        [property: JsonPropertyName("playerIds")] IReadOnlyList<int> PlayerIds
+    );
+}
---
+++ Bandai/GevoApi/Models/Response/PlayerInformation/Advanced.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using System.Text.Json.Serialization;
+
+namespace Bandai.GevoApi.Models.Response.PlayerInformation
+{
+    public record Player(
+        [property: JsonPropertyName("playerId")] int PlayerId,
+        [property: JsonPropertyName("playerName")] string PlayerName,
+        [property: JsonPropertyName("playerLevel")] int PlayerLevel,
+        [property: JsonPropertyName("playerIconItemId")] string PlayerIconItemId,
+        [property: JsonPropertyName("portraitItemId")] string PortraitItemId,
+        [property: JsonPropertyName("trophyItemId")] string TrophyItemId,
+        [property: JsonPropertyName("matchingArea")] int MatchingArea,
+        [property: JsonPropertyName("accountId")] object AccountId /* Likely int, unused on Steam */
+    );
+    public record Advanced(
+        [property: JsonPropertyName("players")] IReadOnlyList<Player> Players
+    );
+}
